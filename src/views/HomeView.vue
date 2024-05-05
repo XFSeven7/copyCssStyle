48ed96a2b8e10db2ec708b2cb17dc13102a6d0d8
qxf <xfseven7@foxmail.com>
2024-05-05 17:41:15 +0400

    删除无用代码

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-<!--    <img alt="Vue logo" src="../assets/logo.png">-->
     <div>
       <label>
         Switch:
@@ -18,13 +17,4 @@ import {ref} from "vue";
 
 const isChecked = ref(false);
 
-// import { Options, Vue } from 'vue-class-component';
-// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-//
-// @Options({
-//   components: {
-//     HelloWorld,
-//   },
-// })
-// export default class HomeView extends Vue {}
 </script>
