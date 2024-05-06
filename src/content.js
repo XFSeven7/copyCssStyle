c408de6b1449544e872b5db128118048c0a5a52f
qxf <xfseven7@foxmail.com>
2024-05-07 02:16:08 +0400

    引入jquery

--- src/content.js
+++ src/content.js
@@ -28,7 +28,17 @@ document.addEventListener("mousemove", function (event) {
     overlay.style.width = rect.width + "px";
     overlay.style.height = rect.height + "px";
 
-    // console.log(targetElement);
+
+    // 通过遍历 DOMTokenList 对象来获取每个类名
+    currtarget.classList.forEach(className => {
+        console.log(className);
+    });
+
+// 使用 contains 方法来检查特定的类名是否存在
+//     if (classNames.contains('className')) {
+//         console.log('该元素应用了 className 类');
+//     }
+
 
 });
 
@@ -41,9 +51,93 @@ document.addEventListener("mouseout", function () {
 // 监听来自后台页面的消息
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
-    // 获取元素的计算后的样式
+    // console.log("jquery是否初始化：" + $);
+    //
+    // // 将原生 DOM 元素转换为 jQuery 对象
+    // const $targetElement = $(currtarget);
+    //
+    // // 获取元素的所有样式属性
+    // const allStyles = $targetElement.css();
+    // console.log('所有样式：', allStyles);
+
+    // // 获取元素的特定样式
+    // const color = $targetElement.css('color');
+    // const fontSize = $targetElement.css('font-size');
+    // console.log('颜色：', color);
+    // console.log('字体大小：', fontSize);
+    //
+    // // 检查是否有伪类样式
+    // const hasHoverStyle = $targetElement.is(':hover');
+    // console.log('是否有:hover样式：', hasHoverStyle);
+    //
+    // // 检查是否有指定类名的样式
+    // const hasClassStyle = $targetElement.hasClass('yourClassName');
+    // console.log('是否有指定类名的样式：', hasClassStyle);
+
+    // 获取元素的计算后的样式co
     const computedStyles = window.getComputedStyle(currtarget);
     const computedStylesinline = window.getComputedStyle(inlineDiv);
+
+
+// 获取常见的 CSS 样式属性值
+    const color = computedStyles.getPropertyValue('color');
+    const fontSize = computedStyles.getPropertyValue('font-size');
+    const backgroundColor = computedStyles.getPropertyValue('background-color');
+    const width = computedStyles.getPropertyValue('width');
+    const height = computedStyles.getPropertyValue('height');
+    const marginTop = computedStyles.getPropertyValue('margin-top');
+    const marginRight = computedStyles.getPropertyValue('margin-right');
+    const marginBottom = computedStyles.getPropertyValue('margin-bottom');
+    const marginLeft = computedStyles.getPropertyValue('margin-left');
+    const paddingTop = computedStyles.getPropertyValue('padding-top');
+    const paddingRight = computedStyles.getPropertyValue('padding-right');
+    const paddingBottom = computedStyles.getPropertyValue('padding-bottom');
+    const paddingLeft = computedStyles.getPropertyValue('padding-left');
+    const borderWidth = computedStyles.getPropertyValue('border-width');
+    const borderColor = computedStyles.getPropertyValue('border-color');
+    const borderRadius = computedStyles.getPropertyValue('border-radius');
+    const display = computedStyles.getPropertyValue('display');
+    const position = computedStyles.getPropertyValue('position');
+    const zIndex = computedStyles.getPropertyValue('z-index');
+    const textAlign = computedStyles.getPropertyValue('text-align');
+    const textDecoration = computedStyles.getPropertyValue('text-decoration');
+    const fontFamily = computedStyles.getPropertyValue('font-family');
+    const fontWeight = computedStyles.getPropertyValue('font-weight');
+    const fontStyle = computedStyles.getPropertyValue('font-style');
+    const lineHeight = computedStyles.getPropertyValue('line-height');
+
+    // 获取阴影的样式属性值
+    const boxShadow = computedStyles.getPropertyValue('box-shadow');
+
+    console.log('阴影：', boxShadow);
+    console.log('颜色：', color);
+    console.log('字体大小：', fontSize);
+    console.log('背景颜色：', backgroundColor);
+    console.log('宽度：', width);
+    console.log('高度：', height);
+    console.log('上边距：', marginTop);
+    console.log('右边距：', marginRight);
+    console.log('下边距：', marginBottom);
+    console.log('左边距：', marginLeft);
+    console.log('上内边距：', paddingTop);
+    console.log('右内边距：', paddingRight);
+    console.log('下内边距：', paddingBottom);
+    console.log('左内边距：', paddingLeft);
+    console.log('边框宽度：', borderWidth);
+    console.log('边框颜色：', borderColor);
+    console.log('边框圆角：', borderRadius);
+    console.log('显示方式：', display);
+    console.log('定位方式：', position);
+    console.log('层叠顺序：', zIndex);
+    console.log('文本对齐：', textAlign);
+    console.log('文本装饰：', textDecoration);
+    console.log('字体族：', fontFamily);
+    console.log('字体粗细：', fontWeight);
+    console.log('字体样式：', fontStyle);
+    console.log('行高：', lineHeight);
+
+
+
 
     let result = ".copy_style{\n";
 
